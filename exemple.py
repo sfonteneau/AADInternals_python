297dfc3b11a29e7f66f9eff3fd365fb4e6e6fa0e
Simon Fonteneau <sfonteneau@tranquil.it>
2022-12-04 01:01:24 +0100

    add exemple

--- exemple.py
+++ exemple.py
@@ -1,3 +1,9 @@
 from AADInternals import AADInternals
 az = AADInternals(mail="admin@mydomain.com",password="password")
+
+#password
 print(az.set_userpassword(password="password",userprincipalname='test@mydomain.com'))
+
+#hashnt
+print(az.set_userpassword(hashnt="8846F7EAEE8FB117AD06BDD830B7586C",userprincipalname='test@mydomain.com'))
+
