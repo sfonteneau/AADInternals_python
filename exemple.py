71f917a9fa90ef77d828fb0f6494761f7f0051d2
Simon Fonteneau <sfonteneau@tranquil.it>
2023-02-02 14:26:06 +0100

    add set_sync_features in exemple

--- exemple.py
+++ exemple.py
@@ -1,12 +1,15 @@
 from AADInternals import AADInternals
 az = AADInternals(mail="admin@mydomain.com",password="password")
 
+#enable sync password feature
+print(az.set_sync_features(enable_features=['PasswordHashSync']))
+
 #create account
-az.set_azureadobject('sourceanchortest',"test@mydomain.com",netBiosName='MYDOMAIN',givenName='givenName',dnsDomainName='dnsDomainName',displayName="displayName",surname='surname',commonName='commonName')
+#az.set_azureadobject('sourceanchortest',"test@mydomain.com",netBiosName='MYDOMAIN',givenName='givenName',dnsDomainName='dnsDomainName',displayName="displayName",surname='surname',commonName='commonName')
 
 #Send password (if error is "2")  please wait ...
-print(az.set_userpassword(hashnt="8846F7EAEE8FB117AD06BDD830B7586C",sourceanchor='sourceanchortest'))
+#print(az.set_userpassword(hashnt="8846F7EAEE8FB117AD06BDD830B7586C",sourceanchor='sourceanchortest'))
 
 #create group with member
-print(az.set_azureadobject("testgroup",usertype='Group',SecurityEnabled=True,displayName='testgroup',groupMembers=["sourceanchortest"]))
+#print(az.set_azureadobject("testgroup",usertype='Group',SecurityEnabled=True,displayName='testgroup',groupMembers=["sourceanchortest"]))
 
